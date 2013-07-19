521ed428f845caa9e02ddf1e6b6881043d426564
geekete <agneseklints@gmail.com>
2013-07-19 15:40:23 +0300

    Extra Row preferences and strings added/changed

--- src/com/project/latviankeyboard/PrefsExtraRow.java
+++ src/com/project/latviankeyboard/PrefsExtraRow.java
@@ -2,6 +2,9 @@ package com.project.latviankeyboard;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 
 public class PrefsExtraRow extends PreferenceActivity {
 	
@@ -10,7 +13,15 @@ public class PrefsExtraRow extends PreferenceActivity {
 		
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.prefs_extra_row);
-		
+
 	}
 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		Log.i("PrefList", "pos: "+position+" id: "+id);
+	}
+
+	
+	
 }
