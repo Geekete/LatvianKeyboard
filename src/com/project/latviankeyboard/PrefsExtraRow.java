37f34783d795fdabc3770b7b429abfcaf5acd4e7
geekete <agneseklints@gmail.com>
2013-07-23 15:31:25 +0300

    3 additional preferences (text/button shadow, text fonts)

--- src/com/project/latviankeyboard/PrefsExtraRow.java
+++ src/com/project/latviankeyboard/PrefsExtraRow.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.PaintDrawable;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
@@ -18,6 +19,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -27,12 +29,15 @@ public class PrefsExtraRow extends PreferenceActivity {
 	SeekBar redBar, greenBar, blueBar, alphaBar, seekBar;
 	View colorTest;
 	LinearLayout colorChooser, seekBarDialog;
+	ScrollView fontChooser;
 	SeekBar.OnSeekBarChangeListener seekBarChangeListener, sbChangeListener;
-	Button btnOk, btnCancel, sbBtnCancel, sbBtnOk;
-	Dialog d, sbD;
+	Button btnOk, btnCancel, sbBtnCancel, sbBtnOk, fcB1, fcB2, fcB3, fcB4, fcB5, fcB6, fcB7;
+	Dialog d, sbD, fcD;
 	SharedPreferences prefs;
 	String prefKey;
 	TextView sbText, textRed, textGreen, textBlue, textAlpha;
+	Typeface font;
+	Button.OnClickListener btnActionListener;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
@@ -42,32 +47,55 @@ public class PrefsExtraRow extends PreferenceActivity {
 		// inflates preferences
 		addPreferencesFromResource(R.xml.prefs_extra_row);
 
-		// inflates colorChooser
+		// inflates colorChooser and its objects
 		colorChooser = (LinearLayout) getLayoutInflater().inflate(R.layout.color_chooser, null);
-		// inflates seekBarDialog
-		seekBarDialog = (LinearLayout) getLayoutInflater().inflate(R.layout.seek_bar_dialog, null);
-
-		// gets shared preferences to edit them later
-		prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		final SharedPreferences.Editor editor = prefs.edit();
-
-		// gets seekbars and textView
 		redBar = (SeekBar) colorChooser.findViewById(R.id.sbRed);
 		greenBar = (SeekBar) colorChooser.findViewById(R.id.sbGreen);
 		blueBar = (SeekBar) colorChooser.findViewById(R.id.sbBlue);
 		alphaBar = (SeekBar) colorChooser.findViewById(R.id.sbAlpha);
-		seekBar = (SeekBar) seekBarDialog.findViewById(R.id.seekBar);
-		sbText = (TextView) seekBarDialog.findViewById(R.id.textRed);
 		textRed = (TextView) colorChooser.findViewById((R.id.textRed));
 		textGreen = (TextView) colorChooser.findViewById((R.id.textGreen));
 		textBlue = (TextView) colorChooser.findViewById((R.id.textBlue));
 		textAlpha = (TextView) colorChooser.findViewById((R.id.textAlpha));
-
-		// inflates ok and cancel buttons
 		btnOk = (Button) colorChooser.findViewById(R.id.btnOk);
 		btnCancel = (Button) colorChooser.findViewById(R.id.btnCancel);
+
+		// inflates seekBarDialog and its objects
+		seekBarDialog = (LinearLayout) getLayoutInflater().inflate(R.layout.seek_bar_dialog, null);
+		seekBar = (SeekBar) seekBarDialog.findViewById(R.id.seekBar);
+		sbText = (TextView) seekBarDialog.findViewById(R.id.textRed);
 		sbBtnOk = (Button) seekBarDialog.findViewById(R.id.sbBtnOk);
 		sbBtnCancel = (Button) seekBarDialog.findViewById(R.id.sbBtnCancel);
+
+		// inflates fontChooser
+		fontChooser = (ScrollView) getLayoutInflater().inflate(R.layout.font_chooser, null);
+		fcB1 = (Button) fontChooser.findViewById(R.id.fcBtnFont1);
+		fcB2 = (Button) fontChooser.findViewById(R.id.fcBtnFont2);
+		fcB3 = (Button) fontChooser.findViewById(R.id.fcBtnFont3);
+		fcB4 = (Button) fontChooser.findViewById(R.id.fcBtnFont4);
+		fcB5 = (Button) fontChooser.findViewById(R.id.fcBtnFont5);
+		fcB6 = (Button) fontChooser.findViewById(R.id.fcBtnFont6);
+		fcB7 = (Button) fontChooser.findViewById(R.id.fcBtnFont7);
+
+		// sets fonts to buttons in fontChooser
+		font = Typeface.createFromAsset(getAssets(), "fonts/Xolonium-Regular.otf");
+		fcB1.setTypeface(font);
+		font = Typeface.createFromAsset(getAssets(), "fonts/GrandHotel-Regular.otf");
+		fcB2.setTypeface(font);
+		font = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
+		fcB3.setTypeface(font);
+		font = Typeface.createFromAsset(getAssets(), "fonts/Pecita.otf");
+		fcB4.setTypeface(font);
+		font = Typeface.createFromAsset(getAssets(), "fonts/d-puntillas-D-to-tiptoe.ttf");
+		fcB5.setTypeface(font);
+		font = Typeface.createFromAsset(getAssets(), "fonts/Megrim.ttf");
+		fcB6.setTypeface(font);
+		font = Typeface.createFromAsset(getAssets(), "fonts/Jura-Medium.ttf");
+		fcB7.setTypeface(font);
+
+		// gets shared preferences to edit them later
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		final SharedPreferences.Editor editor = prefs.edit();
 
 		// inflates Dialog context and adds colorChooser layout
 		d = new Dialog(PrefsExtraRow.this);
@@ -78,6 +106,38 @@ public class PrefsExtraRow extends PreferenceActivity {
 		sbD = new Dialog(PrefsExtraRow.this);
 		sbD.setContentView(seekBarDialog);
 		sbD.setCanceledOnTouchOutside(true);
+
+		// inflates Dialog context and adds fontChooser layout
+		fcD = new Dialog(PrefsExtraRow.this);
+		fcD.setContentView(fontChooser);
+		fcD.setCanceledOnTouchOutside(true);
+
+		// fontChooser button click listener
+		btnActionListener = new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (v.getId() == fcB1.getId()) {
+					editor.putString("erTextFont", "fonts/Xolonium-Regular.otf");
+				} else if (v.getId() == fcB2.getId()) {		
+					editor.putString("erTextFont", "fonts/GrandHotel-Regular.otf");
+				} else if (v.getId() == fcB3.getId()) {
+					editor.putString("erTextFont", "fonts/Roboto-Regular.ttf");
+				} else if (v.getId() == fcB4.getId()) {
+					editor.putString("erTextFont", "fonts/Pecita.otf");
+				} else if (v.getId() == fcB5.getId()) {
+					editor.putString("erTextFont", "fonts/d-puntillas-D-to-tiptoe.ttf");
+				} else if (v.getId() == fcB6.getId()) {
+					editor.putString("erTextFont", "fonts/Megrim.ttf");
+				} else if (v.getId() == fcB7.getId()) {
+					editor.putString("erTextFont", "fonts/Jura-Medium.ttf");
+				} else {
+					Log.i("Bļum bļum", "DANGER!");
+				}
+				editor.commit();
+				fcD.dismiss();
+			}
+		};
 
 		// ColorChooser action listener
 		seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
@@ -161,13 +221,22 @@ public class PrefsExtraRow extends PreferenceActivity {
 				sbD.dismiss();
 			}
 		});
-		
+
 		// adds actionListener to seekbars
 		redBar.setOnSeekBarChangeListener(seekBarChangeListener);
 		greenBar.setOnSeekBarChangeListener(seekBarChangeListener);
 		blueBar.setOnSeekBarChangeListener(seekBarChangeListener);
 		alphaBar.setOnSeekBarChangeListener(seekBarChangeListener);
 		seekBar.setOnSeekBarChangeListener(sbChangeListener);
+		
+		//set click listener to fontChooser buttons
+		fcB1.setOnClickListener(btnActionListener);
+		fcB2.setOnClickListener(btnActionListener);
+		fcB3.setOnClickListener(btnActionListener);
+		fcB4.setOnClickListener(btnActionListener);
+		fcB5.setOnClickListener(btnActionListener);
+		fcB6.setOnClickListener(btnActionListener);
+		fcB7.setOnClickListener(btnActionListener);
 
 		// inflates view that is used to test color
 		colorTest = colorChooser.findViewById(R.id.testColor);
@@ -204,9 +273,10 @@ public class PrefsExtraRow extends PreferenceActivity {
 					greenBar.setProgress(Color.green(bkColor));
 					blueBar.setProgress(Color.blue(bkColor));
 					alphaBar.setProgress(Color.alpha(bkColor));
-					textRed.setText(""+redBar.getProgress());
-					textGreen.setText(""+greenBar.getProgress());
-					textBlue.setText(""+blueBar.getProgress());
+					textRed.setText("" + redBar.getProgress());
+					textGreen.setText("" + greenBar.getProgress());
+					textBlue.setText("" + blueBar.getProgress());
+					textAlpha.setText("" + alphaBar.getProgress());
 					d.show();
 				} else if (prefKey.equals("erBtnBackground")) {
 					d.setTitle(R.string.titleBtnBackground);
@@ -216,9 +286,23 @@ public class PrefsExtraRow extends PreferenceActivity {
 					greenBar.setProgress(Color.green(bkColor));
 					blueBar.setProgress(Color.blue(bkColor));
 					alphaBar.setProgress(Color.alpha(bkColor));
-					textRed.setText(""+redBar.getProgress());
-					textGreen.setText(""+greenBar.getProgress());
-					textBlue.setText(""+blueBar.getProgress());
+					textRed.setText("" + redBar.getProgress());
+					textGreen.setText("" + greenBar.getProgress());
+					textBlue.setText("" + blueBar.getProgress());
+					textAlpha.setText("" + alphaBar.getProgress());
+					d.show();
+				} else if (prefKey.equals("erBtnShadow")) {
+					d.setTitle(R.string.titleBtnShadow);
+					int bkColor = prefs.getInt(prefKey, Color.argb(170, 0, 0, 0));
+					colorTest.setBackgroundColor(bkColor);
+					redBar.setProgress(Color.red(bkColor));
+					greenBar.setProgress(Color.green(bkColor));
+					blueBar.setProgress(Color.blue(bkColor));
+					alphaBar.setProgress(Color.alpha(bkColor));
+					textRed.setText("" + redBar.getProgress());
+					textGreen.setText("" + greenBar.getProgress());
+					textBlue.setText("" + blueBar.getProgress());
+					textAlpha.setText("" + alphaBar.getProgress());
 					d.show();
 				} else if (prefKey.equals("erBtnHoverColor")) {
 					d.setTitle(R.string.titleBtnHoverColor);
@@ -228,9 +312,10 @@ public class PrefsExtraRow extends PreferenceActivity {
 					greenBar.setProgress(Color.green(bkColor));
 					blueBar.setProgress(Color.blue(bkColor));
 					alphaBar.setProgress(Color.alpha(bkColor));
-					textRed.setText(""+redBar.getProgress());
-					textGreen.setText(""+greenBar.getProgress());
-					textBlue.setText(""+blueBar.getProgress());
+					textRed.setText("" + redBar.getProgress());
+					textGreen.setText("" + greenBar.getProgress());
+					textBlue.setText("" + blueBar.getProgress());
+					textAlpha.setText("" + alphaBar.getProgress());
 					d.show();
 				} else if (prefKey.equals("erBtnBorderColor")) {
 					d.setTitle(R.string.titleBtnBorderColor);
@@ -240,9 +325,10 @@ public class PrefsExtraRow extends PreferenceActivity {
 					greenBar.setProgress(Color.green(bkColor));
 					blueBar.setProgress(Color.blue(bkColor));
 					alphaBar.setProgress(Color.alpha(bkColor));
-					textRed.setText(""+redBar.getProgress());
-					textGreen.setText(""+greenBar.getProgress());
-					textBlue.setText(""+blueBar.getProgress());
+					textRed.setText("" + redBar.getProgress());
+					textGreen.setText("" + greenBar.getProgress());
+					textBlue.setText("" + blueBar.getProgress());
+					textAlpha.setText("" + alphaBar.getProgress());
 					d.show();
 				} else if (prefKey.equals("erBtnTextColor")) {
 					d.setTitle(R.string.titleBtnTextColor);
@@ -252,10 +338,27 @@ public class PrefsExtraRow extends PreferenceActivity {
 					greenBar.setProgress(Color.green(bkColor));
 					blueBar.setProgress(Color.blue(bkColor));
 					alphaBar.setProgress(Color.alpha(bkColor));
-					textRed.setText(""+redBar.getProgress());
-					textGreen.setText(""+greenBar.getProgress());
-					textBlue.setText(""+blueBar.getProgress());
+					textRed.setText("" + redBar.getProgress());
+					textGreen.setText("" + greenBar.getProgress());
+					textBlue.setText("" + blueBar.getProgress());
+					textAlpha.setText("" + alphaBar.getProgress());
 					d.show();
+				} else if (prefKey.equals("erTextShadow")) {
+					d.setTitle(R.string.titleTextShadow);
+					int bkColor = prefs.getInt(prefKey, Color.argb(135, 0, 0, 0));
+					colorTest.setBackgroundColor(bkColor);
+					redBar.setProgress(Color.red(bkColor));
+					greenBar.setProgress(Color.green(bkColor));
+					blueBar.setProgress(Color.blue(bkColor));
+					alphaBar.setProgress(Color.alpha(bkColor));
+					textRed.setText("" + redBar.getProgress());
+					textGreen.setText("" + greenBar.getProgress());
+					textBlue.setText("" + blueBar.getProgress());
+					textAlpha.setText("" + alphaBar.getProgress());
+					d.show();
+				} else if (prefKey.equals("erTextFont")) {
+					fcD.setTitle(R.string.titleTextFont);
+					fcD.show();
 				} else if (prefKey.equals("erTextSize")) {
 					sbD.setTitle(R.string.titleTextSize);
 					seekBar.setProgress(prefs.getInt(prefKey, 25));
@@ -284,9 +387,12 @@ public class PrefsExtraRow extends PreferenceActivity {
 					editor.remove("erWaitTime");
 					editor.remove("erBackgroundColor");
 					editor.remove("erBtnBackground");
+					editor.remove("erBtnShadow");
 					editor.remove("erBtnHoverColor");
 					editor.remove("erBtnBorderColor");
 					editor.remove("erBtnTextColor");
+					editor.remove("erTextShadow");
+					editor.remove("erTextFont");
 					editor.remove("erTextSize");
 					editor.remove("erBtnPadding");
 					editor.remove("erBtnRoundness");
@@ -302,9 +408,12 @@ public class PrefsExtraRow extends PreferenceActivity {
 		findPreference("erWaitTime").setOnPreferenceClickListener(myListener);
 		findPreference("erBackgroundColor").setOnPreferenceClickListener(myListener);
 		findPreference("erBtnBackground").setOnPreferenceClickListener(myListener);
+		findPreference("erBtnShadow").setOnPreferenceClickListener(myListener);
 		findPreference("erBtnHoverColor").setOnPreferenceClickListener(myListener);
 		findPreference("erBtnBorderColor").setOnPreferenceClickListener(myListener);
 		findPreference("erBtnTextColor").setOnPreferenceClickListener(myListener);
+		findPreference("erTextShadow").setOnPreferenceClickListener(myListener);
+		findPreference("erTextFont").setOnPreferenceClickListener(myListener);
 		findPreference("erTextSize").setOnPreferenceClickListener(myListener);
 		findPreference("erBtnPadding").setOnPreferenceClickListener(myListener);
 		findPreference("erBtnRoundness").setOnPreferenceClickListener(myListener);
